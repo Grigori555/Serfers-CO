92d0f60229715782f502b563f64ddfff8b09093e
grisha <morozehs@yandex.ru>
2020-08-07 19:42:42 +0300

    fixed 1 page part 1

---
+++ text-переработка 2 страницы.js
@@ -0,0 +1,30 @@
+background: url("../images/intro/intro.jpg");
+    background-repeat:no-repeat;
+    background-position:top;
+    background-size: 100% 600px;
+
+ <div class="about__content">
+                    <div class="about__background">
+                        <img src="assets/images/about-us/Photo.jpg" alt="">
+                    </div>
+                        <div class="about__text">
+                            <div class="about__text-container">
+                                    <h3>We are Surfers co.</h3>
+                                    <p class="about__story">Lorem ipsum dolor sit amet, 
+                                        aeneam consectetur adipiscing elit. 
+                                        Fusce iaculis feugiat arcunum fermentum 
+                                        hendrerit. Suspendisse auctor labor nisi, 
+                                        et interdum diam facilisis, aliquam pulvinar 
+                                        semi egetis belle sollicitudin ut. Nullam tincidun 
+                                        finibustus scelerisque.
+                                    </p>
+
+                                    <a href="#" class="about__read">Read more</a>
+                                </div>        
+                        </div>
+                        <div class="about__video">
+                            <img src="assets/images/about-us/surfers.png">
+                        </div>
+
+
+    
