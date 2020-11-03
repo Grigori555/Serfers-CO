9e58ec7571e7f605058607ddf35b719dd7f87b54
grisha <morozehs@yandex.ru>
2020-11-03 09:36:24 +0300

    script for serfboard frame v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,21 @@ const abBtnCl = document.querySelector(".about-close");
 const updItems = document.querySelectorAll(".update__item");
 
 
+const vieImgItms = document.querySelectorAll(".views__img-item");
 
+
+/* 1 page Serfboard slaider*/
+for(let i = 0; i<=vieImgItms.length; i++){
+  
+    vieImgItms[i].addEventListener("click",()=>{
+        vieImgItms[i].classList.remove("active");
+        if(vieImgItms[i].classList.contains("active")){
+            vieImgItms[i].classList.remove("active");
+        }else{
+            vieImgItms[i].classList.add("active")
+        }
+    });
+}
 /* 2 page latest updates */
 abBtnCl.addEventListener("click",()=>{
    console.log("hello");
