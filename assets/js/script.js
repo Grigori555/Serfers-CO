d5b23505512fef90e55cb3fd04e7cb43613496e9
grisha <morozehs@yandex.ru>
2020-11-03 11:48:31 +0300

    finish responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,36 @@ const vieImgItms = document.querySelectorAll(".views__img-item");
 
 
 /* 1 page Serfboard slaider*/
-for(let i = 0; i<=vieImgItms.length; i++){
-  
-    vieImgItms[i].addEventListener("click",()=>{
-        vieImgItms[i].classList.remove("active");
-        if(vieImgItms[i].classList.contains("active")){
+// vieImgItms.forEach(function(index){
+//     index.addEventListener("click",()=>{
+//         index.classList.toggle("active");
+//     });
+// });
+
+
+for(let j =0; j<=vieImgItms.length-1; j++){
+    vieImgItms[j].addEventListener("click",()=>{
+        vieImgItms[j].classList.add("active");
+        for(let i =j+1; i<=vieImgItms.length-1; i++){
             vieImgItms[i].classList.remove("active");
-        }else{
-            vieImgItms[i].classList.add("active")
-        }
+        };
+        for(let k =vieImgItms.length-vieImgItms[j]; k>=0; k--){
+            
+           console.log("1"+ vieImgItms[k]);
+        };
+       
     });
 }
+
+console.log(vieImgItms.length);
+
+// for(let k =vieImgItms.length-3; k>=0; k--){
+            
+//     console.log(vieImgItms[k].classList.add("active"));
+// };
+
+
+
 /* 2 page latest updates */
 abBtnCl.addEventListener("click",()=>{
    console.log("hello");
